6a4f4725f21da742726510e915592957a1c8a81d
orhanbalci <orhanbalci@gmail.com>
2015-12-31 13:39:19 +0200

    Update math_helper.rs
    mod definitions removed 
    combinations funstion made more efficient

--- math_helper.rs
+++ math_helper.rs
@@ -1,34 +1,40 @@
-mod math_helper {
-    fn combinations(n: u32, k: u32) -> u32 {
-        assert!(n >= k);
-        let upper_part = (n - k + 1..n + 1).fold(1_u32, |mut accu, x| {
-            accu = accu * x;
-            accu
-        });
-        upper_part / factorial(k)
+
+pub fn combinations(n: u32, k: u32) -> f32 {
+    assert!(n >= k);
+    if n == k {
+        return 1f32
     }
 
-    fn factorial(n: u32) -> u32 {
-        (0..n + 1).fold(1_u32, |mut accu, x| {
-            match x {
-                0 => accu = 1,
-                _ => accu = accu * x,
-            }
-            accu
-        })
-    }
+    let it = (1..k+1).rev();
+    let combinations = (n - k + 1..n + 1).rev().zip(it).fold(1_f32, |mut accu, x| {
+        accu *= x.0 as f32 ;
+        accu /= x.1 as f32;
+        accu
+    });
+    combinations
+}
 
-    mod test {
-        #[test]
-        fn test_factorial() {
-            assert_eq!(super::factorial(0), 1_u32);
-            assert_eq!(super::factorial(1), 1_u32);
-            assert_eq!(super::factorial(5), 120_u32);
+pub fn factorial(n: u32) -> f32 {
+    (0..n + 1).fold(1_f32, |mut accu, x| {
+        match x {
+            0 => accu = 1_f32,
+            _ => accu = accu * x as f32,
         }
+        accu
+    })
+}
 
-        #[test]
-        fn test_combination() {
-            assert_eq!(super::combinations(5, 2), 10_u32);
-        }
+mod test {
+    #[test]
+    fn test_factorial() {
+        assert_eq!(super::factorial(0), 1_f32);
+        assert_eq!(super::factorial(1), 1_f32);
+        assert_eq!(super::factorial(5), 120_f32);
+    }    
+
+    #[test]
+    fn test_combination() {
+        assert_eq!(super::combinations(5, 2), 10_f32);
+        assert_eq!(super::combinations(6, 4), 15_f32);
     }
 }
