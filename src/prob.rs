ec92dab202ff98642b2fd16e4d4b1964116fae51
orhanbalci <=>
2016-04-21 23:50:20 +0300

    some warnigs fixed on prob.rs

--- src/prob.rs
+++ src/prob.rs
@@ -1,4 +1,3 @@
-
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
@@ -14,46 +13,46 @@ fn main() {
         // The `description` method of `io::Error` returns a string that
         // describes the error
         Err(why) => panic!("couldn't open {}: {}", display,
-                                                   Error::description(&why)),
-        Ok(file) => file,
+                           Error::description(&why)),
+                           Ok(file) => file,
     };
 
     // Read the file contents into a string, returns `io::Result<usize>`
     let mut s = String::new();
     match file.read_to_string(&mut s) {
         Err(why) => panic!("couldn't read {}: {}", display,
-                                                   Error::description(&why)),
-        Ok(_) => print!("{} contains:\n{}", display, s),
+                           Error::description(&why)),
+                           Ok(_) => print!("{} contains:\n{}", display, s),
     }
 
     let vec: Vec<&str> = s.split("\n").collect();
 
-    let mut freqAT : i32 =0;
-    let mut freqGC : i32 =0;
+    let mut freq_at : i32 =0;
+    let mut freq_gc : i32 =0;
     for c in vec[0].chars(){
         match c {
-            'A'|'T' => freqAT += 1,
-            'G'|'C' => freqGC += 1,
-             _ => {}
+            'A'|'T' => freq_at += 1,
+            'G'|'C' => freq_gc += 1,
+            _ => {}
         }
     }
 
     let mut vec2 : Vec<f64> = vec[1].split(" ").map(|s| s.trim()).map(|s| s.parse().unwrap()).collect();
 
-    vec2 = vec2.into_iter().map(|gcContent| computeLog(freqGC,freqAT,gcContent)).collect();
+    vec2 = vec2.into_iter().map(|gc_content| compute_log(freq_gc,freq_at,gc_content)).collect();
     for element in &vec2 {
         print!("{} ",element);
     }
 
 }
 
-fn computeLog(gcFreq : i32, atFreq : i32, gcContent : f64) ->f64{
-        let mut result : f64 = 0.0;
-        let mut probAT : f64 = 0.0;
-        let mut probGC : f64 = 0.0;
+fn compute_log(gc_freq : i32, at_freq : i32, gc_content : f64) ->f64{
+    let result : f64;
+    let prob_at : f64;
+    let prob_gc : f64;
 
-        probGC = gcContent / 2.0;
-        probAT = ((1.0 - gcContent)/2.0);
-        result = probAT.powi(atFreq) * probGC.powi(gcFreq);
-        result.log10()
+    prob_gc = gc_content / 2.0;
+    prob_at = (1.0 - gc_content)/2.0;
+    result = prob_at.powi(at_freq) * prob_gc.powi(gc_freq);
+    result.log10()
 }
