5518b80e6053e5cee3619d71fe072e49b3942781
orhanbalci <orhanbalci@gmail.com>
2016-04-25 11:10:41 +0300

    rust fmt run

--- src/pdst.rs
+++ src/pdst.rs
@@ -4,15 +4,22 @@ use std::fmt;
 fn main() {
     let dnas = protein_utilities::read_fasta("./data/pdst.txt");
     let mut matrix_1d = vec![0.0; dnas.len() * dnas.len()];
-    let mut row_collection =  matrix_1d.chunks_mut(dnas.len()).collect::<Vec<_>>();
+    let mut row_collection = matrix_1d.chunks_mut(dnas.len()).collect::<Vec<_>>();
     let mut matrix = row_collection.as_mut_slice();
-    dnas.iter().enumerate().inspect(|index_first_dna|{
-        dnas.iter().skip(index_first_dna.0).enumerate().inspect(|index2_second_dna|{
-            let distance = distance(index_first_dna.1, index2_second_dna.1);
-            matrix[index_first_dna.0][index_first_dna.0+index2_second_dna.0] = distance;
-            matrix[index_first_dna.0+index2_second_dna.0][index_first_dna.0] = distance;
-        }).collect::<Vec<_>>();
-    }).collect::<Vec<_>>();
+    dnas.iter()
+        .enumerate()
+        .inspect(|index_first_dna| {
+            dnas.iter()
+                .skip(index_first_dna.0)
+                .enumerate()
+                .inspect(|index2_second_dna| {
+                    let distance = distance(index_first_dna.1, index2_second_dna.1);
+                    matrix[index_first_dna.0][index_first_dna.0 + index2_second_dna.0] = distance;
+                    matrix[index_first_dna.0 + index2_second_dna.0][index_first_dna.0] = distance;
+                })
+                .collect::<Vec<_>>();
+        })
+        .collect::<Vec<_>>();
 
     pretty_print(matrix);
 }
@@ -27,11 +34,15 @@ fn distance(first_string: &str, second_string: &str) -> f32 {
     different_char_count as f32 / first_string.len() as f32
 }
 
-fn pretty_print<T : fmt::Display>(matrix : &mut[&mut[T]]){
-    matrix.iter().inspect(|row|{
-        row.iter().inspect(|element|{
-            print!("{} ",element);
-        }).collect::<Vec<_>>();
-        println!("");
-    }).collect::<Vec<_>>();
+fn pretty_print<T: fmt::Display>(matrix: &mut [&mut [T]]) {
+    matrix.iter()
+          .inspect(|row| {
+              row.iter()
+                 .inspect(|element| {
+                     print!("{} ", element);
+                 })
+                 .collect::<Vec<_>>();
+              println!("");
+          })
+          .collect::<Vec<_>>();
 }
