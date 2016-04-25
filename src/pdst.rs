77d1eadf498bc81cc4f25b84767db869f2bd837c
orhanbalci <orhanbalci@gmail.com>
2016-04-25 11:07:26 +0300

    pdst problem solution added

--- src/pdst.rs
+++ src/pdst.rs
@@ -1,5 +1,20 @@
+// http://rosalind.info/problems/pdst
+mod protein_utilities;
+use std::fmt;
 fn main() {
+    let dnas = protein_utilities::read_fasta("./data/pdst.txt");
+    let mut matrix_1d = vec![0.0; dnas.len() * dnas.len()];
+    let mut row_collection =  matrix_1d.chunks_mut(dnas.len()).collect::<Vec<_>>();
+    let mut matrix = row_collection.as_mut_slice();
+    dnas.iter().enumerate().inspect(|index_first_dna|{
+        dnas.iter().skip(index_first_dna.0).enumerate().inspect(|index2_second_dna|{
+            let distance = distance(index_first_dna.1, index2_second_dna.1);
+            matrix[index_first_dna.0][index_first_dna.0+index2_second_dna.0] = distance;
+            matrix[index_first_dna.0+index2_second_dna.0][index_first_dna.0] = distance;
+        }).collect::<Vec<_>>();
+    }).collect::<Vec<_>>();
 
+    pretty_print(matrix);
 }
 
 fn distance(first_string: &str, second_string: &str) -> f32 {
@@ -10,4 +25,13 @@ fn distance(first_string: &str, second_string: &str) -> f32 {
                                            .len();
 
     different_char_count as f32 / first_string.len() as f32
+}
+
+fn pretty_print<T : fmt::Display>(matrix : &mut[&mut[T]]){
+    matrix.iter().inspect(|row|{
+        row.iter().inspect(|element|{
+            print!("{} ",element);
+        }).collect::<Vec<_>>();
+        println!("");
+    }).collect::<Vec<_>>();
 }
